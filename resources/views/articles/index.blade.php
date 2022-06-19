8e7f85e5d15f80e6abfa4755b26d52c7753476da
moutoon <moutoonm342@gmail.com>
2022-06-19 18:05:20 +0900

    add: #2 記事一覧画面とナビバーの作成
        共通のビューとナビバーのビューを作成した

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,21 +1,32 @@
 @extends('app')
 
+@section('title', '記事一覧')
+
 @section('content')
+@include('nav')
 
 <div class="container">
 
-        @foreach ($articles as $article)
-        <div class="card mt-3">
-            <div class="card-body">
-                <div class="fw-bold">{{ $article->user_id }}</div>
-                <div class="card-text">{{ $article->created_at }}</div>
+    @foreach($articles as $article)
+    <div class="card mt-3">
+            <div class="card-body d-flex flex-row">
+                <i class="fas fa-user-circle fa-3x mr-1"></i>
+                <div>
+                    <div class="font-weight-bold">
+                    {{ $article->user_id }}
+                    </div>
+                    <div class="font-weight-lighter">
+                    {{ $article->created_at->format('Y/m/d H:i') }}
+                    </div>
+                </div>
             </div>
-            <div class="card-body pt-0 pb-3">
-                <div class="card-text">{{ $article->body }}</div>
+            <div class="card-body pt-0 pb-2">
+                <div class="card-text">
+                    {!! nl2br(e( $article->body )) !!}
+                </div>
             </div>
         </div>
-        @endforeach
+    @endforeach
 
 </div>
-
 @endsection
